079a0dd93249c7e9374b867e275ad878baa940bd
Aaron D Borden <adborden@a14n.net>
2017-02-17 16:40:35 -0800

    Plumbing for FormStore

--- static_src/constants.js
+++ static_src/constants.js
@@ -36,6 +36,15 @@ const errorActionTypes = keymirror({
   DISMISS: null
 });
 
+const formActionTypes = keymirror({
+  // User has changed a form field
+  FORM_FIELD_CHANGE: null,
+  // The form field change was valid
+  FORM_FIELD_CHANGE_SUCCESS: null,
+  // The form field change is not a valid input
+  FORM_FIELD_CHANGE_ERROR: null
+});
+
 const loginActionTypes = keymirror({
   // Action of fetching a login status, whether the user is logged in or not.
   FETCH_STATUS: null,
@@ -293,6 +302,7 @@ export {
   activityActionTypes,
   appActionTypes,
   entityHealth,
+  formActionTypes,
   appStates,
   domainActionTypes,
   errorActionTypes,
