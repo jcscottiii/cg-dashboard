1e739aa7710a8a652cfd2e63e65febcb1df64729
James C. Scott <jamesc.scott@gsa.gov>
2017-07-09 11:25:53 -0400

    add more test commands

--- ci/build.sh
+++ ci/build.sh
@@ -3,4 +3,6 @@ source /docker-lib.sh
 start_docker
 docker-compose run --rm frontend npm run build
 docker-compose run --rm frontend npm run test
+docker-compose run --rm frontend npm run test-performance
+docker-compose run --rm backend ./codecheck.sh
 docker volume rm $(docker volume ls -q)
