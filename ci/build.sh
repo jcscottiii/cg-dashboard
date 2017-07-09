1662ef9a5d5e85b40483c1a124ee06b92406e02c
James C. Scott <jamesc.scott@gsa.gov>
2017-07-09 10:30:33 -0400

    use dcind

--- ci/build.sh
+++ ci/build.sh
@@ -1,28 +1,6 @@
 #!/bin/sh
-# Install docker-compose:
-docker_install(){
-  apk add --no-cache py-pip curl
-  pip install docker-compose
-}
-
-# Set up docker daemon with a config file:
-docker_config(){
-  mkdir /etc/docker
-  touch /etc/docker/daemon.json
-  echo '{"storage-driver":"vfs","debug":true}' > /etc/docker/daemon.json
-}
-
-# Start the docker daemon as a background task:
-start_daemon(){
-  #dockerd --config-file=/etc/docker/daemon.json -p /var/run/docker-bootstrap.pid &
-  service docker start
-  service docker status
-  sleep 30
-}
-
-docker_install
-# docker_config
-start_daemon
-
+source /docker-lib.sh
+start_docker
 docker-compose run --rm frontend npm run build
 docker-compose run --rm frontend npm run test
+docker volume rm $(docker volume ls -q)
