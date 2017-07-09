515284949a83cb79687284b4f42e108037d424c7
James C. Scott <jamesc.scott@gsa.gov>
2017-07-09 09:48:38 -0400

    get rid of bad stuff from copying

--- ci/build.sh
+++ ci/build.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 # Install docker-compose:
 docker_install(){
-    apk add --no-cache py-pip curl
-    pip install docker-compose
+  apk add --no-cache py-pip curl
+  pip install docker-compose
 }
-‌‌
+
 # Set up docker daemon with a config file:
 docker_config(){
-    mkdir /etc/docker
-    touch /etc/docker/daemon.json
-    echo '{"storage-driver":"btrfs","debug":true}' > 		/etc/docker/daemon.json
+  mkdir /etc/docker
+  touch /etc/docker/daemon.json
+  echo '{"storage-driver":"btrfs","debug":true}' > /etc/docker/daemon.json
 }
-‌‌
+
 # Start the docker daemon as a background task:
 start_daemon(){
-    dockerd --config-	file=/etc/docker/daemon.json -p /var/run/docker-bootstrap.pid &
+  dockerd --config-file=/etc/docker/daemon.json -p /var/run/docker-bootstrap.pid &
 }
-‌‌
+
 docker_install
 docker_config
 start_daemon
