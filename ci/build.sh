2efcb0ca85877d302c5a41c98e8e73465b87deaf
James C. Scott <jamesc.scott@gsa.gov>
2017-07-09 11:31:52 -0400

    add go build command and set -e

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+set -e
 source /docker-lib.sh
 start_docker
 docker-compose run --rm frontend npm run build
 docker-compose run --rm frontend npm run test
 docker-compose run --rm frontend npm run test-performance
+docker-compose run --rm backend go build
 docker-compose run --rm backend ./codecheck.sh
 docker volume rm $(docker volume ls -q)
