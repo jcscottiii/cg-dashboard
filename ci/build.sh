1ceebe6370f720661b694ef74cc82c3626abbc02
James C. Scott <jamesc.scott@gsa.gov>
2017-07-10 14:56:02 -0400

    make sure all the containers are down and out

--- ci/build.sh
+++ ci/build.sh
@@ -7,4 +7,5 @@ docker-compose run --rm frontend npm run test
 docker-compose run --rm frontend npm run test-performance
 docker-compose run --rm backend go build
 docker-compose run --rm backend ./codecheck.sh
+docker-compose rm -v -s -f
 docker volume rm $(docker volume ls -q)
