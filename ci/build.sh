4d3f1412420e5ea60f730304e99b58b34d506154
James C. Scott <jamesc.scott@gsa.gov>
2017-07-10 16:02:24 -0400

    fix build info

--- ci/build.sh
+++ ci/build.sh
@@ -3,12 +3,13 @@ set -e
 source /docker-lib.sh
 start_docker
 # Get build info
-pip install --user ruamel.yaml
 TAG=$(git describe $(git rev-list --tags --max-count=1))
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export BUILD_INFO=build::$BRANCH::$TAG::$(date -u "+%Y-%m-%d-%H-%M-%S")::$BUILD_PIPELINE_NAME::$BUILD_JOB_NAME::$BUILD_NAME::$(ci/scripts/npm-version.sh)
 echo $BUILD_INFO
-python ci/scripts/vars-to-manifest.py
+wget https://github.com/mikefarah/yaml/releases/download/1.11/yaml_linux_amd64 -O yaml-cli
+chmod +x yaml-cli
+./yaml-cli write -i manifests/manifest-base.yml env.BUILD_INFO $BUILD_INFO
 # run tests
 docker-compose run --rm frontend npm run build
 docker-compose run --rm frontend npm run test
